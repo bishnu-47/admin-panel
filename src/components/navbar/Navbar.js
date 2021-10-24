b0ce121640d14fc76cb1506944c9a2aadaec6690
bishnu-47 <bd23122000@gmail.com>
2021-10-24 08:34:45 +0530

    init commit
    - added material ui library
    - did file cleanup

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { AppBar } from "@mui/material";
+
+const Navbar = () => {
+  return (
+    <div>
+      <AppBar position="static" size="large">
+        Logo
+      </AppBar>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ src/screens/users/UsersScreen.js
@@ -0,0 +1,7 @@
+import Navbar from "../../components/navbar/Navbar";
+
+const UsersScreen = () => {
+  return <Navbar />;
+};
+
+export default UsersScreen;
